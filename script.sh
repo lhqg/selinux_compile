b4ac67013b7d72dfe65913e8958343068886a27a
Hubert Quarantel-Colombani <hubert@quarantel.name>
2024-09-30 15:32:35 +0200

    Created parametrized Docker image

---
+++ script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+unset ID
+
+. /etc/os-release 
+
+case  "${ID}" in
+	"debian"|"ubuntu")
+		apt-get update -yqq
+		apt-get install -yqq --no-install-recommends \
+			selinux-basics selinux-policy-dev policycoreutils make
+	;;
+	"fedora"|"rhel"|"almalinux"|"centos"|"rockylinux")
+		dnf install -y selinux-policy-devel make
+		dnf upgrade -y libsepol
+	;;
+	*)
+		echo "FATAL: distribution ${DISTRO} is not supported."
+	;;
+esac
+
+# Compile the SELinux code
+if [ -d /source/se_module ]
+then
+	cd /source/se_module
+	make -f /usr/share/selinux/devel/Makefile
+else
+	echo "FATAL: cannot enter /source/se_module directory."
+	exit 1
+fi
